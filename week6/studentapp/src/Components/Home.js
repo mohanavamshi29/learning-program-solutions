8e2904ea2a4f547c967140edaa0bfe2e5fed2783
vamshi <mohanavamshi29@gmail.com>
2025-07-27 09:03:06 +0530

    Week-6

---
+++ week6/studentapp/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Home page of Mohana vamshi's Student Management Portal</h2>
+    </div>
+  );
+}
+
+export default Home;
